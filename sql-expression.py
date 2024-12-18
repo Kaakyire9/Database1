8ccf5ea921cef3f915e6a2ee68a6f3c83c3f2f31
(Sampson) Reginald <r.aduboffour@gmail.com>
2024-12-18 20:33:28 +0000

    Fix foreign key references in SQLAlchemy table definitions and update select queries in sql-expression.py

--- sql-expression.py
+++ sql-expression.py
@@ -1,11 +1,12 @@
 from sqlalchemy import (
     create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
 )
+from sqlalchemy.future import select
 
 # executing the instructions from our localhost "chinook" db
 db = create_engine("postgresql:///chinook")
 
-meta = MetaData(db)
+meta = MetaData()
 
 #create variable for "Artist" table
 artist_table = Table(
@@ -19,7 +20,7 @@ album_table = Table(
     "Album", meta, 
     Column("AlbumId", Integer, primary_key=True),
     Column("Title", String),
-    Column("ArtistId",Integer, ForeignKey("artist_table.ArtistId"))
+    Column("ArtistId", Integer, ForeignKey("Artist.ArtistId"))
 )
 
 # create Variable for "Track" table 
@@ -27,9 +28,9 @@ track_table = Table(
     "Track", meta,
     Column("TrackId", Integer, primary_key=True),
     Column("Name", String),
-    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
-    Column("MediaTypeId", Integer,primary_key=False),
-    Column("GenreId", Integer, primary_key = False),
+    Column("AlbumId", Integer, ForeignKey("Album.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
     Column("Composer", String),
     Column("Milliseconds", Integer),
     Column("Bytes", Integer),
@@ -39,8 +40,25 @@ track_table = Table(
 # making the connection
 with db.connect() as connection:
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
 
-    results = connection. execute(select_query)
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns(artist_table.c.Name)
+
+    # Query 3 - select only "Queen" from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 - select only "ArtistId" from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - select only "Album" from the "ArtistId" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 _ select all records from the "Track" table
+    select_query = track_table.select()
+
+
+
+    results = connection.execute(select_query)
     for result in results: 
         print(result)
